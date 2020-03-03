9dc7836fdbd41a38344ee7e1e2e5f3a2518e3374
TimDovg <timdovg@gmail.com>
2020-03-03 14:09:15 +0200

    adapted

--- js/task_2.js
+++ js/task_2.js
@@ -1,7 +1,3 @@
-function delay(time) {
-    return new Promise((resolve, reject) => {
-        setTimeout(resolve, time, time);
-    });
-}
+const delay = time => new Promise(resolve => setTimeout(resolve, time, time));
 
 delay(1000).then(value => console.log(`Done with ${value}`));
