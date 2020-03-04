07c21f8a23e831b5f5d20ec888719d8f54345e44
TimDovg <timdovg@gmail.com>
2020-03-04 15:42:51 +0200

    adapted

--- js/task_6.js
+++ js/task_6.js
@@ -1,11 +1,11 @@
 const getResolvedPromise = value => Promise.resolve(value)
     .then(value => {
         if (value > 300) {
-            console.log(new Error(`error: value > 300`));
+            return new Error(`error: value > 300`);
         }
-
-        console.log(`Promise done`);
 
         return value;
     })
+    .catch(console.log)
+    .finally(() => console.log(`Promise done`))
 ;
