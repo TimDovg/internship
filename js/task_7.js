057952ba1e3ea64c8f766e7a0aceeedb0ddd7d16
TimDovg <timdovg@gmail.com>
2020-03-04 17:29:03 +0200

    duplicate, call & apply & bind polyfills

---
+++ js/task_7.js
@@ -0,0 +1,19 @@
+Array.prototype.duplicate = function() {
+    return [...this];
+};
+
+Function.prototype.call = function (obj, ...args) {
+    obj.func = this;
+    obj.func(...args);
+};
+
+Function.prototype.apply = function (obj, args) {
+    obj.func = this;
+    obj.func(...args);
+};
+
+Function.prototype.bind = function (obj) {
+    const myFunction = this;
+
+    return (...args) => (() => myFunction.apply(obj, args))();
+};
