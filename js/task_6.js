6e46835fb71b81382ed77365334ebe8d5647c3ca
TimDovg <timdovg@gmail.com>
2020-03-04 15:00:01 +0200

    adapted

--- js/task_6.js
+++ js/task_6.js
@@ -1,7 +1,11 @@
-const getResolvedPromise = status => {
-    if (status > 300) {
-        console.log(new Error(`error: status > 300`));
-    }
+const getResolvedPromise = url => {
+    fetch(url)
+        .then(({status}) => {
+            if (status > 300) {
+                console.log(new Error(`error: status > 300`));
+            }
 
-    console.log(`Promise done`);
+            console.log(`Promise done`);
+    })
+    ;
 };
