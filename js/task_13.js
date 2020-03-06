8f25a6156f1bfcbe2118c0c879b54e6d723d3438
TimDovg <timdovg@gmail.com>
2020-03-06 12:22:03 +0200

    sum function

---
+++ js/task_13.js
@@ -0,0 +1,18 @@
+const sum = a => {
+    let count = a;
+    const func = b => {
+        if (b === undefined) return count;
+
+        count += b;
+
+        return func;
+    };
+
+    func.toString = () => count;
+
+    return func;
+};
+
+console.log(sum(1)(2)(3)()); //6
+console.log(sum(1)(2)(3)(4) + 1); //11
+console.log(sum(1)(2)(3)(4)(5) + 1); //16
